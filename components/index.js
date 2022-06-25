09788183e36f2801dec597b764e3cccdfd3f2305
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-06-25 18:07:24 +0100

    feat: global export

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Cart } from './Cart'
+export { default as CartItem } from './CartItem'
+export { default as ProductCard } from './ProductCart'
+export { default as Search } from './Search'
