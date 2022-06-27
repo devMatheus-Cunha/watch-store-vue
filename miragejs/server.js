eafb7b1fb23ad5d4a41510706f2cd5ffdc860c3d
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-06-27 11:51:31 +0100

    feat: config server

---
+++ miragejs/server.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import {
+    Server
+} from 'miragejs'
+import products from '@/mocks/products.json'
+
+export const makeServer = ({
+    environment = 'development'
+} = {}) => {
+    return new Server({
+        environment,
+        routes() {
+            this.namespace = 'api';
+
+            this.get('products', () => ({
+                products,
+            }))
+        },
+    })
+}
