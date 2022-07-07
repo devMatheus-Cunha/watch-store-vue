f81d8002282d46a7fc3c7a3bd5f3442581bd9eb6
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-07-07 11:29:57 +0100

    refactor: new config routes and seeds mirage

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,65 @@
 <template>
     <main class="my-8">
-        <search />
-        <div class="container mx-auto px-6">
+        <search @doSearch="setSearchTerm" />
+        <div v-if="errorMessage === ''" class="container mx-auto px-6">
             <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
             <span
                 data-testid="total-quantity-label"
                 class="mt-3 text-sm text-gray-500"
-                >8 produtos</span
+                >{{ quantityLabel }}</span
             >
             <div
                 class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
             >
                 <product-card
-                    v-for="product in products"
+                    v-for="product in list"
                     :key="product.id"
                     :product="product"
+                    data-testid="product-card"
                 />
             </div>
         </div>
-        <!-- <h3 class=":text-center text-2x1">{{ errorMessage }}</h3> -->
+        <h3 v-else class="text-center text-2x1">{{ errorMessage }}</h3>
     </main>
 </template>
 
 <script>
 import { ProductCard, Search } from '../components';
-import products from '@/mocks/products.json';
 
 export default {
     components: { ProductCard, Search },
     data() {
         return {
             products: [],
+            errorMessage: '',
+            searchTerm: '',
         };
     },
+    computed: {
+        list() {
+            if (this.searchTerm !== '')
+                return this.products.filter(({ title }) =>
+                    title.includes(this.searchTerm)
+                );
+
+            return this.products;
+        },
+        quantityLabel() {
+            const {
+                list: { length },
+            } = this;
+
+            return length === 1 ? `${length} Product` : `${length} Products`;
+        },
+    },
     async created() {
-        this.products = (await this.$axios.get('/api/products')).data.products;
-        await this.$axios.get('/api/users');
+        try {
+            this.products = (
+                await this.$axios.get('/api/products')
+            ).data.products;
+        } catch (error) {
+            this.errorMessage = 'Problemas ao carregar a lista!';
+        }
     },
 };
 </script>
