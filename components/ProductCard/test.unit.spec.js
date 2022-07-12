62f81144c3f710e7058ce2f69f65112210dbc0fb
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-07-12 12:18:33 +0100

    feat: should emit the event addToCart with product obj when button gets clicked

--- components/ProductCard/test.unit.spec.js
+++ components/ProductCard/test.unit.spec.js
@@ -11,15 +11,20 @@ import {
 } from "@/miragejs/server"
 
 const mountProductCard = () => {
-    return mount(ProductCard, {
-        propsData: {
-            product: server.create("product", {
-                title: "Relogio bonito",
-                price: "22.00",
-                image: "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-            })
-        }
+    const product = server.create("product", {
+        title: "Relogio bonito",
+        price: "22.00",
+        image: "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
     })
+    return {
+        wrapper: mount(ProductCard, {
+            propsData: {
+                product,
+            }
+        }),
+        product,
+
+    }
 }
 
 describe('ProductCard - unit', () => {
@@ -29,21 +34,40 @@ describe('ProductCard - unit', () => {
             environment: 'test'
         })
     })
-
     afterEach(() => {
         server.shutdown()
     })
+
     it('should match snapshot', () => {
-        const wrapper = mountProductCard()
+        const {
+            wrapper
+        } = mountProductCard()
 
         expect(wrapper.element).toMatchSnapshot()
     });
 
     it('should mount the component', () => {
-        const wrapper = mountProductCard()
+        const {
+            wrapper
+        } = mountProductCard()
 
         expect(wrapper.vm).toBeDefined()
         expect(wrapper.text()).toContain("Relogio bonito")
         expect(wrapper.text()).toContain("$22.00")
+    });
+
+    it('should emit the event addToCart with product obj when button gets clicked,', async () => {
+        const {
+            wrapper,
+            product
+        } = mountProductCard()
+
+        await wrapper.find("button").trigger('click')
+
+        expect(wrapper.emitted().addToCart).toBeTruthy()
+        expect(wrapper.emitted().addToCart.length).toBe(1)
+        expect(wrapper.emitted().addToCart[0]).toEqual([{
+            product
+        }])
     });
 });
