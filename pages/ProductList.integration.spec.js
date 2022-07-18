542fc94852239c8793acb12c77b0fbd77ee66f87
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-07-18 11:43:03 +0100

    feat: add test should  display the error message when Promise rejects

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -78,4 +78,17 @@ describe('ProductList - integration', () => {
         expect(cards).toHaveLength(10)
     });
 
+    it('should  display the error message when Promise rejects', async () => {
+        axios.get.mockReturnValue(Promise.reject(new Error("Problemas ao carregar a lista!")))
+
+        const wrapper = mount(ProductList, {
+            mocks: {
+                $axios: axios
+            }
+        })
+
+        await Vue.nextTick()
+        expect(wrapper.text()).toContain("Problemas ao carregar a lista!")
+    });
+
 })
