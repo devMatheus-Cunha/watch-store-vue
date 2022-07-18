fb2ba89b5e2f0a836d32f2b8d9697947a8c3e400
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-07-18 11:04:09 +0100

    feat: add test should call axios.get on component mount

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -2,6 +2,9 @@ import {
     mount
 } from "@vue/test-utils"
 
+// Utils
+import axios from 'axios'
+
 // Components
 import {
     ProductCard,
@@ -9,15 +12,38 @@ import {
 } from '../components'
 import ProductList from '.'
 
+jest.mock('axios', () => ({
+    get: jest.fn(),
+}))
+
 describe('ProductList - integration', () => {
     it('should mount the component', () => {
         const wrapper = mount(ProductList)
         expect(wrapper.vm).toBeDefined()
     });
 
-    it('should mount the Search as a child', () => {
-        const wrapper = mount(Search)
+    it('should mount the Search component', () => {
+        const wrapper = mount(ProductList)
         expect(wrapper.findComponent(Search)).toBeDefined()
     });
+
+    it('should call axios.get on component mount', () => {
+        mount(ProductList, {
+            mocks: {
+                $axios: axios
+            }
+        })
+
+        expect(axios.get).toHaveBeenCalledTimes(1)
+        expect(axios.get).toHaveBeenCalledWith("/api/products")
+    });
+
+    // it('should mount the ProductCard component 10 times', () => {
+    //     const wrapper = mount(ProductList)
+    //     const cards = wrapper.findAllComponents(ProductCard)
+
+
+    //     expect(cards).toHaveLength(10)
+    // });
 
 })
