196353ed6beb520d1dfbb14ed8c4d7495e568f53
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-08-03 10:20:30 +0100

    feat: add todo tests in the CartManager

---
+++ managers/CartManager.unit.spec.js
@@ -0,0 +1,19 @@
+describe('CartManager', () => {
+    it.todo('should return the state')
+
+    it.todo('should set cart to open')
+
+    it.todo('should set cart to closed')
+
+    it.todo('should add product to the cart only once')
+
+    it.todo('should remove product from the cart')
+
+    it.todo('should clear products')
+
+    it.todo('should clear cart')
+
+    it.todo('should return true if cart is not empty')
+
+    it.todo('should return true if product is already in the cart')
+})
