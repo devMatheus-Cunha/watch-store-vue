bb6e2edd1beb0d0a5704c4f19495296c6f2f36cb
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-08-10 12:03:53 +0100

    refactor: structure test and add test should add item to cartState on button click

--- components/ProductCard/test.unit.spec.js
+++ components/ProductCard/test.unit.spec.js
@@ -1,43 +1,53 @@
+// Packages
 import {
     mount
-} from "@vue/test-utils"
+} from '@vue/test-utils'
+import {
+    CartManager
+} from '@/managers/CartManager'
 
 // Components
-import ProductCard from "@/components/ProductCard"
+import ProductCart from '.'
 
 // Services
 import {
     makeServer
-} from "@/miragejs/server"
+} from '@/miragejs/server'
 
-import {
-    cartState
-} from "@/state"
-
-const mountProductCard = () => {
-    const product = server.create("product", {
-        title: "Relogio bonito",
-        price: "22.00",
-        image: "https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+const mountProductCart = () => {
+    const product = server.create('product', {
+        title: 'Relógio bonito',
+        price: '25,90',
+        image: 'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
     })
-    return {
-        wrapper: mount(ProductCard, {
-            propsData: {
-                product,
-            }
-        }),
-        product,
 
+    const cartManager = new CartManager()
+
+    const wrapper = mount(ProductCart, {
+        propsData: {
+            product,
+        },
+        mocks: {
+            $cart: cartManager,
+        },
+    })
+
+    return {
+        wrapper,
+        product,
+        cartManager,
     }
 }
 
-describe('ProductCard - unit', () => {
+describe('ProductCart - Unit', () => {
     let server
+
     beforeEach(() => {
         server = makeServer({
             environment: 'test'
         })
     })
+
     afterEach(() => {
         server.shutdown()
     })
@@ -45,29 +55,47 @@ describe('ProductCard - unit', () => {
     it('should match snapshot', () => {
         const {
             wrapper
-        } = mountProductCard()
+        } = mountProductCart()
 
         expect(wrapper.element).toMatchSnapshot()
-    });
+    })
 
     it('should mount the component', () => {
         const {
             wrapper
-        } = mountProductCard()
+        } = mountProductCart()
 
         expect(wrapper.vm).toBeDefined()
-        expect(wrapper.text()).toContain("Relogio bonito")
-        expect(wrapper.text()).toContain("$22.00")
-    });
+        expect(wrapper.text()).toContain('Relógio bonito')
+        expect(wrapper.text()).toContain('$25,90')
+    })
 
-    // it('should add item to cart state on butt onclick', async () => {
-    //     const {
-    //         wrapper,
-    //     } = mountProductCard()
+    it('should add item to cartState on button click', async () => {
+        const {
+            wrapper,
+            cartManager
+        } = mountProductCart()
 
-    //     await wrapper.find("button").trigger('click')
-    //     expect(cartState.items).toHaveLength(1)
-    // });
+        const openSpy = jest.spyOn(cartManager, 'open')
+        const addProductSpy = jest.spyOn(cartManager, 'addProduct')
 
-    it.todo("should ensure product is not added to the cart twice ")
-});
+        await wrapper.find('button').trigger('click')
+
+        expect(openSpy).toHaveBeenCalledTimes(1)
+        expect(addProductSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('addProduct should been called with correctly product', async () => {
+        const {
+            wrapper,
+            cartManager,
+            product
+        } = mountProductCart()
+
+        const addProductSpy = jest.spyOn(cartManager, 'addProduct')
+
+        await wrapper.find('button').trigger('click')
+
+        expect(addProductSpy).toHaveBeenCalledWith(product)
+    })
+})
