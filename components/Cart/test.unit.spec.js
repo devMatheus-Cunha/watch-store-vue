a6fa9c9c550277b64905a420b7d953550e9bc65e
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-08-11 11:57:20 +0100

    refactor: structure of the tests in Cart

--- components/Cart/test.unit.spec.js
+++ components/Cart/test.unit.spec.js
@@ -1,6 +1,10 @@
+// Packages
 import {
     mount
 } from '@vue/test-utils'
+import {
+    CartManager
+} from '@/managers/CartManager'
 
 // Components
 import Cart from '.'
@@ -25,58 +29,123 @@ describe('Cart', () => {
     afterEach(() => {
         server.shutdown()
     })
-    it('should mount the component', async () => {
-        const wrapper = mount(Cart)
+
+    const mountCart = (quantity = 2) => {
+        const products = server.createList('product', quantity)
+
+        const cartManager = new CartManager()
+
+        const wrapper = mount(Cart, {
+            propsData: {
+                products,
+            },
+            mocks: {
+                $cart: cartManager,
+            },
+        })
+
+        return {
+            wrapper,
+            cartManager,
+            products
+        }
+    }
+
+    it('should mount the component', () => {
+        const {
+            wrapper
+        } = mountCart()
 
         expect(wrapper.vm).toBeDefined()
-    });
+    })
+
+    it('should not display empty cart button when there are no products', () => {
+        const {
+            cartManager
+        } = mountCart()
+
+        const wrapper = mount(Cart, {
+            mocks: {
+                $cart: cartManager,
+            },
+        })
+
+        expect(wrapper.find('[data-testid="clear-cart-button"]').exists()).toBe(
+            false
+        )
+    })
 
     it('should emit close event when button gets clicked', async () => {
-        const wrapper = mount(Cart)
+        const {
+            wrapper
+        } = mountCart()
         const button = wrapper.find('[data-testid="close-button"]')
 
         await button.trigger('click')
 
         expect(wrapper.emitted().close).toBeTruthy()
         expect(wrapper.emitted().close).toHaveLength(1)
-    });
+    })
 
-    it('should hide the cart when no prop isOpen is passed', async () => {
-        const wrapper = mount(Cart)
+    it('should hide the cart when no prop isOpen is passed', () => {
+        const {
+            wrapper
+        } = mountCart()
 
         expect(wrapper.classes()).toContain('hidden')
-    });
+    })
 
     it('should display the cart when prop isOpen is passed', async () => {
-        const wrapper = mount(Cart, {
-            propsData: {
-                isOpen: true
-            }
+        const {
+            wrapper
+        } = mountCart()
+
+        await wrapper.setProps({
+            isOpen: true,
         })
 
         expect(wrapper.classes()).not.toContain('hidden')
-    });
+    })
 
-    it('should display "Cart is empty" when there are no products', async () => {
-        const wrapper = mount(Cart, {
-            propsData: {
-                isOpen: true
-            }
-        })
+    it('should display "Cart is empty" when there are no products', () => {
+        const {
+            wrapper
+        } = mountCart(0)
 
         expect(wrapper.text()).toContain('Cart is empty')
-    });
+    })
 
-
-    it('should display 2 instances of CartItem when 2 products are provided', async () => {
+    it('should display 2 instances of CartItem when 2 products are provided', () => {
         const products = server.createList('product', 2)
         const wrapper = mount(Cart, {
             propsData: {
-                products
-            }
+                products,
+            },
         })
 
         expect(wrapper.findAllComponents(CartItem)).toHaveLength(2)
         expect(wrapper.text()).not.toContain('Cart is empty')
-    });
-});
+    })
+
+    it('should display a button to clear cart', () => {
+        const {
+            wrapper
+        } = mountCart()
+
+        const button = wrapper.find('[data-testid="clear-cart-button"]')
+
+        expect(button.exists()).toBe(true)
+    })
+
+    it('should call cart manager clearProducts() when button gets clicked', async () => {
+        const {
+            wrapper,
+            cartManager
+        } = mountCart()
+
+        const spy = jest.spyOn(cartManager, 'clearProducts')
+        await wrapper.find('[data-testid="clear-cart-button"]').trigger('click')
+
+        expect(spy).toHaveBeenCalledTimes(1)
+    })
+})
